0ffe654565c2a3ece343825a201ed8357010c0e4
HenrikSwahn <h_swahn@hotmail.com>
2015-05-11 14:35:19 +0200

    Added elements to the score panel

--- src/GUI/GameGUI/ScorePanel.java
+++ src/GUI/GameGUI/ScorePanel.java
@@ -1,7 +1,10 @@
 package GUI.GameGUI;
 
+import javafx.scene.text.*;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.Font;
 
 /**
  * Created by Nilj on 26/04/15.
@@ -10,14 +13,34 @@ public class ScorePanel extends JPanel {
 
     private JLabel playerLabel;
     private JLabel opponentLabel;
+    private JLabel playerScore;
+    private JLabel opponenScore;
+    private JLabel playerTurn;
+    private JLabel opponentTurn;
     private GridBagConstraints gbc;
 
     public ScorePanel() {
 
-        playerLabel = new JLabel("Dummy1");
-        opponentLabel = new JLabel("Dummy2");
+        playerLabel = new JLabel("-");
+        opponentLabel = new JLabel("-");
+        playerScore = new JLabel("-");
+        opponenScore = new JLabel("-");
+        playerTurn = new JLabel("-");
+        opponentTurn = new JLabel("-");
         gbc = new GridBagConstraints();
+        setLabelAttributes();
         setUpLayout();
+
+    }
+
+    private void setLabelAttributes() {
+
+        playerLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+        opponentLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+        playerScore.setFont(new Font("Arial", Font.PLAIN, 12));
+        opponenScore.setFont(new Font("Arial", Font.PLAIN, 12));
+        playerTurn.setFont(new Font("Arial", Font.PLAIN, 12));
+        opponentTurn.setFont(new Font("Arial", Font.PLAIN, 12));
 
     }
 
@@ -27,13 +50,48 @@ public class ScorePanel extends JPanel {
         gbc.fill = GridBagConstraints.BOTH;
         gbc.gridx = 0;
         gbc.gridy = 0;
-        gbc.weighty = 1.0;
+        gbc.weighty = 0.5;
         gbc.weightx = 1.0;
-        add(playerLabel, gbc);
+        add(createPanel(true), gbc);
 
         gbc.gridy = 1;
-        add(opponentLabel, gbc);
+        add(createPanel(false), gbc);
 
+    }
+
+    private JPanel createPanel(boolean b) {
+
+        JPanel p = new JPanel();
+        p.setLayout(new GridBagLayout());
+        GridBagConstraints p_gbc = new GridBagConstraints();
+
+        p_gbc.fill = GridBagConstraints.BOTH;
+        p_gbc.gridy = 0;
+        p_gbc.gridx = 0;
+        p_gbc.weightx = 1.0;
+        p_gbc.weighty = 0.4;
+
+        if(b)
+            p.add(playerLabel, p_gbc);
+        else
+            p.add(opponentLabel, p_gbc);
+
+        p_gbc.gridy = 1;
+        p_gbc.weighty = 0.3;
+
+        if(b)
+            p.add(playerScore, p_gbc);
+        else
+            p.add(opponenScore, p_gbc);
+
+        p_gbc.gridy = 2;
+
+        if(b)
+            p.add(playerTurn, p_gbc);
+        else
+            p.add(opponentTurn, p_gbc);
+
+        return p;
     }
 
     private void setPlayerMark(String player) {
@@ -48,10 +106,34 @@ public class ScorePanel extends JPanel {
 
     }
 
+    public void setPlayerScore(JLabel playerScore) {
+
+        this.playerScore = playerScore;
+
+    }
+
+    public void setOpponenScore(JLabel opponenScore) {
+
+        this.opponenScore = opponenScore;
+
+    }
+
+    public void setPlayerTurn(JLabel playerTurn) {
+
+        this.playerTurn = playerTurn;
+
+    }
+
+    public void setOpponentTurn(JLabel opponentTurn) {
+
+        this.opponentTurn = opponentTurn;
+
+    }
+
     public void setMarks(String playerMark, String opponentMark) {
 
-        setPlayerMark("You are: " + playerMark);
-        setOpponentMark("Opponent: " + opponentMark);
+        setPlayerMark("Your mark: " + playerMark);
+        setOpponentMark("Opponent mark: " + opponentMark);
 
     }
 }
